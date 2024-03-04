67019c247f28944cd3d98d333a763fc539d6b3f0
Iris Huang <lyaui@outlook.com>
2024-03-05 01:47:31 +0800

    feat: Add Project Modal

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -3,7 +3,7 @@ import { useQuery } from '@apollo/client';
 
 import { GET_PROJECT } from '../queries/projectQueries';
 import Spinner from '../components/Spinner';
-// import ClientInfo from '../components/ClientInfo';
+import ClientInfo from '../components/ClientInfo';
 // import DeleteProjectButton from '../components/DeleteProjectButton';
 // import EditProjectForm from '../components/EditProjectForm';
 
@@ -31,11 +31,11 @@ function Project() {
           <h5 className='mt-3'>Project Status</h5>
           <p className='lead'>{data.project.status}</p>
 
-          {/* <ClientInfo client={data.project.client} />
+          <ClientInfo client={data.project.client} />
 
-          <EditProjectForm project={data.project} />
+          {/* <EditProjectForm project={data.project} />
 
-          <DeleteProjectButton projectId={data.project.id} /> */}
+          <DeleteProjectButton projectId={data.project.id} />  */}
         </div>
       )}
     </>
